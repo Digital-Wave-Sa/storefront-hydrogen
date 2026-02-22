924f9b6142a211f56dd8ba213d775a0495268031
motasemodeh <motasem.udeh@gmail.com>
2026-02-22 09:56:05 +0300

    some fixes

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import * as build from 'virtual:react-router/server-build';
+// @ts-ignore
 import {createRequestHandler, storefrontRedirect} from '@shopify/hydrogen';
 import {createHydrogenRouterContext} from '~/lib/context';
 

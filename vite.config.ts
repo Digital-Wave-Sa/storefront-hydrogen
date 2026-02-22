b43b018fa667ab1aa468726b621854b20583b233
motasemodeh <motasem.udeh@gmail.com>
2026-02-22 03:01:51 +0300

    fixx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'vite';
-import {hydrogen} from '@shopify/hydrogen/vite';
-// import {oxygen} from '@shopify/mini-oxygen/vite';
-import {reactRouter} from '@react-router/dev/vite';
+import { defineConfig } from 'vite';
+import { hydrogen } from '@shopify/hydrogen/vite';
+import { reactRouter } from '@react-router/dev/vite';
+import { vercelPreset } from '@vercel/react-router/vite'; // Import this
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -9,31 +9,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     hydrogen(),
-    // oxygen(),
+    vercelPreset(),
     reactRouter(),
     tsconfigPaths(),
   ],
   build: {
-    // Allow a strict Content-Security-Policy
-    // withtout inlining assets as base64:
     assetsInlineLimit: 0,
   },
   ssr: {
     optimizeDeps: {
-      /**
-       * Include dependencies here if they throw CJS<>ESM errors.
-       * For example, for the following error:
-       *
-       * > ReferenceError: module is not defined
-       * >   at /Users/.../node_modules/example-dep/index.js:1:1
-       *
-       * Include 'example-dep' in the array below.
-       * @see https://vitejs.dev/config/dep-optimization-options
-       */
       include: ['set-cookie-parser', 'cookie', 'react-router'],
     },
-  },
-  server: {
-    allowedHosts: ['.tryhydrogen.dev'],
   },
 });
